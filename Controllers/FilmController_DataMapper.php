78bd8f03eab2d9a67a2efa2b8869169208c1929d
Matthew Mantle <m.e.mantle@hud.ac.uk>
2025-10-14 09:13:08 +0100

    Removed solutions

--- Controllers/FilmController_DataMapper.php
+++ Controllers/FilmController_DataMapper.php
@@ -2,15 +2,20 @@
 
 namespace Controllers;
 
-use Models\ActiveRecord\Film;
+use Models\DataMapper\Film;
+use Models\DataMapper\FilmMapper;
 
 class FilmController extends BaseController
 {
-	function __construct() {}
+	private $filmMapper;
+	function __construct()
+	{
+		$this->filmMapper = new FilmMapper();
+	}
 	public function index()
 	{
-		//Get all the films from the model
-		$films = Film::all();
+		//Get all the films
+		$films = $this->filmMapper->findAll();
 		$this->loadView("index.view", ["films" => $films]);
 	}
 	function create()
@@ -22,7 +27,7 @@ class FilmController extends BaseController
 		//Get the id from the query string e.g. for show.php?id=2, $_GET['id'] has a value of 2
 		$id = $_GET['id'];
 		//Get the film from the model
-		$film = Film::find($id);
+		$film = $this->filmMapper->findById($id);
 		$this->loadView("show.view", ["film" => $film]);
 	}
 	function store()
@@ -31,9 +36,10 @@ class FilmController extends BaseController
 		$title = $_POST['title'];
 		$year = $_POST['year'];
 		$duration = $_POST['duration'];
-		//Ask the model to save the new film
+		//Create a new Film object
 		$film = new Film($title, $year, $duration);
-		$film->save();
+		//Ask the mapper to save the film in the database
+		$this->filmMapper->persist($film);
 		//Redirect the user to the home page
 		header('Location: ./index.php');
 	}
